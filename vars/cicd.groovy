19fc49d82d067814c4d7a226432466525bb31d5f
Mvenky564 <98814998+Mvenky564@users.noreply.github.com>
2022-03-31 15:50:39 +0530

    b

---
+++ vars/cicd.groovy
@@ -0,0 +1,16 @@
+def newGir(repo)
+{
+  git "${repo}"
+}
+def newMaven()
+{
+   sh 'mvn package'
+}
+def newDeploy(ip,appname)
+{
+  sh "scp /var/lib/jenkins/workspace/Declarativepipeline/webapp/target/webapp.war cloud_user@${ip}:/var/lib/tomcat9/webapps/${appname}.war"
+}
+def runSelenium(path)
+{
+  sh "java -jar ${path}/testing.jar"
+}
